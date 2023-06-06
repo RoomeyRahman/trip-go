89534af9b3ae1e557e16122c96faa6f102fc463e
arbaucorp <ataur.rahman@pmamediagroup.com>
2023-06-06 10:34:46 +0600

    add filter interceptor and pipes with api swagger

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './common/filters';
+import {
+  LoggingInterceptor,
+  TransformInterceptor,
+} from './common/interceptors';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { UserManagementModule } from './user-management/user-management.module';
@@ -12,6 +18,20 @@ import { UserManagementModule } from './user-management/user-management.module';
     UserManagementModule,
   ],
   controllers: [GatewayController],
-  providers: [GatewayService],
+  providers: [
+    GatewayService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
 })
 export class GatewayModule {}
--- apps/gateway/src/user-management/user-management.controller.ts
+++ apps/gateway/src/user-management/user-management.controller.ts
@@ -1,25 +1,76 @@
-import { Inject, Controller, Body, Post } from '@nestjs/common';
+import {
+  Inject,
+  Controller,
+  Body,
+  Post,
+  HttpStatus,
+  HttpException,
+  UsePipes,
+} from '@nestjs/common';
 import { SERVICE_MESSAGES, USER_MESSAGES } from '@app/shared/constants';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { CreateUserDTO } from 'apps/user-management/src/users/dto';
 import { IUser } from 'apps/user-management/src/users/interfaces';
+import { NullValidationPipe, ValidationPipe, TrimPipe } from '../common/pipes';
+import {
+  ApiBasicAuth,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiOperation,
+  ApiTags,
+  ApiHeader,
+  ApiExcludeEndpoint,
+} from '@nestjs/swagger';
 
+/**
+ * User Controller
+ */
+@ApiTags('User')
+@ApiResponse({
+  status: HttpStatus.METHOD_NOT_ALLOWED,
+  description: 'Method not allowed',
+})
+@ApiResponse({
+  status: HttpStatus.INTERNAL_SERVER_ERROR,
+  description: 'Server Error!',
+})
 @Controller('users')
 export class UserManagementController {
   constructor(
     @Inject(SERVICE_MESSAGES.USER_MANAGEMENT) private client: ClientProxy,
   ) {}
 
+  /**
+   * Create a user
+   * @Body {CreateUserDTO} createUserDTO
+   * @returns {Promise<IUser>} created user data
+   */
+  @ApiOperation({ summary: 'User registration: create new user' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Return new user.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Invalid data',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_ACCEPTABLE,
+    description: 'Record already exist',
+  })
+  @UsePipes(new NullValidationPipe())
+  @UsePipes(new ValidationPipe(true))
+  @UsePipes(new TrimPipe())
   @Post()
   register(
     @Body()
-    userDTO: CreateUserDTO,
+    createUserDTO: CreateUserDTO,
   ): Observable<IUser> {
     try {
-      return this.client.send<IUser>(USER_MESSAGES.REGISTER, userDTO);
+      return this.client.send<IUser>(USER_MESSAGES.REGISTER, createUserDTO);
     } catch (err) {
-      console.log(err);
+      throw new HttpException(err, err.status || HttpStatus.BAD_REQUEST);
     }
   }
 }
